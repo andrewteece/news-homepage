5c0c885383447e1646036dcd934c3f202de6c7df
Andrew Teece <andrewteece@gmail.com>
2023-06-19 17:43:33 -0500

    work on js

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,24 +1,27 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+// NAVIGATION
+
+const mobileNavBtn = document.querySelector('#mobileNavBtn');
+const mobileNav = document.querySelector('#mobileNav');
+const navLinks = document.querySelectorAll(".nav--link");
+const closeBtn = document.querySelector("#closeBtn");
+const navBkg = document.querySelector(".nav--bkg");
+
+const handleMobileNavClick = (e) => {
+  const isOpen = JSON.parse(e.target.getAttribute("aria-expanded"));
+	e.target.setAttribute("aria-expanded", !isOpen);
+	toggleOpenAttribute([mobileNav, navBkg]);
+	toggleTabIndexes([...navLinks, closeBtn]);
+};
+
+const closeMobileNav = () => {
+	mobileNavBtn.setAttribute("aria-expanded", "false");
+	toggleOpenAttribute([mobileNav, navBkg]);
+	toggleTabIndexes([...navLinks, closeBtn]);
+};
+
+// EVENT LISTENERS 
+
+mobileNavBtn.addEventListener('click', handleMobileNavClick);
